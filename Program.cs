40f1b4e739e304f63d2a27d3d22f4261e3ddc9e5
MrTorres30 <torresandelizw@gmail.com>
2026-09-03 04:57:37 -0400

    feat: configure connection string and wire up dependencies in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Data.Common;
+using InventoryX.Repositories;
+using InventoryX.Services;
+using InventoryX.UI;
+
+const string connectionString  =
+"Server=.;Database=InventoryXDb;Trusted_Connection=True;TrustServerCertificate=True;";
+
+IProductRepository repository = new SqlProductRepository(connectionString);
+var inventoryService          = new InventoryService(repository);
+var ui                        = new ConsoleInterface(inventoryService);
+
+ui.Run();
